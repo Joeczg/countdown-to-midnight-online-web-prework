0d4d5e68c4cee97f6ed1e5bf6edb23f8a540e09a
zhenggao cai <caizhengao1992@gmail.com>
2019-04-14 04:55:12 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(time_left)
+  while time_left>0
+  puts "#{time_left} SECOND(S)!"
+  time_left-=1
+end
+puts "HAPPY NEW YEAR!"
+return "HAPPY NEW YEAR!"
+end
+def countdown_with_sleep(count_number)
+  while count_number>0
+  puts "#{count_number} SECOND(S)!"
+  sleep(2)
+  count_number-=1
+end
+puts "HAPPY NEW YEAR!"
+return "HAPPY NEW YEAR!"
 end
